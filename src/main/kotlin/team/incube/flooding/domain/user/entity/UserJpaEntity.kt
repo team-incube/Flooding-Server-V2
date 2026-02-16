4aabe7a567ea666bf422f257073d81821aa48d3f
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-02-16 18:58:36 +0900

    update(user): table, column 이름 명시

--- src/main/kotlin/team/incube/flooding/domain/user/entity/UserJpaEntity.kt
+++ src/main/kotlin/team/incube/flooding/domain/user/entity/UserJpaEntity.kt
@@ -1,18 +1,34 @@
 package team.incube.flooding.domain.user.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "tb_user")
 class UserJpaEntity(
     @field:Id
+    @field:Column(name = "id")
     val id: Long,
+
+    @field:Column(name = "name")
     var name: String,
+
+    @field:Column(name = "sex")
     var sex: Sex,
+
+    @field:Column(name = "email")
     var email: String,
-    var studentNumber: StudentNumber,
+
+    @field:Column(name = "student_number")
+    var studentNumber: Int,
+
+    @field:Column(name = "role")
     var role: Role,
-    var dormitoryRoom: DormitoryRoom
+
+    @field:Column(name = "dormitory_room")
+    var dormitoryRoom: Int
 ) {
     @JvmInline
     value class StudentNumber(val value: Int){
