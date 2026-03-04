3401b8323c00909d8e04148c84883dd522c00d67
C0ng_yun <s25069@gsm.hs.kr>
2026-03-04 18:10:28 +0900

    add : 자습 경로 추가

--- src/main/kotlin/team/incube/flooding/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/team/incube/flooding/global/security/config/SecurityConfig.kt
@@ -1,7 +1,9 @@
 package team.incube.flooding.global.security.config
 
+import io.swagger.v3.oas.models.PathItem
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -19,9 +21,16 @@ class SecurityConfig(
         http
             .csrf { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
             .authorizeHttpRequests {
                 it.requestMatchers("/v2/auth/**").permitAll()
                 it.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+
+                //자습
+                it.requestMatchers(HttpMethod.POST, "/study").hasRole("GENERAL_STUDENT")
+                it.requestMatchers(HttpMethod.DELETE, "/study").hasRole("GENERAL_STUDENT")
+                it.requestMatchers(HttpMethod.POST,"/study/ban").hasAnyRole("DORMITORY_MANAGER", "ADMIN")
                 it.anyRequest().authenticated()
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
