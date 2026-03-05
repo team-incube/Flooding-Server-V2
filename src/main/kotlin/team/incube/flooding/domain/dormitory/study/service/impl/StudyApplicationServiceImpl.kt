32e24d8e1e4ad93f02ea804b035b8c1671a418aa
C0ng_yun <s25069@gsm.hs.kr>
2026-03-05 15:05:19 +0900

    update : 자습 금지 여부를 Redis와 DB 모두 확인하도록 수정

--- src/main/kotlin/team/incube/flooding/domain/dormitory/study/service/impl/StudyApplicationServiceImpl.kt
+++ src/main/kotlin/team/incube/flooding/domain/dormitory/study/service/impl/StudyApplicationServiceImpl.kt
@@ -6,22 +6,25 @@ import org.springframework.transaction.annotation.Transactional
 import team.incube.flooding.domain.dormitory.study.config.StudyProperties
 import team.incube.flooding.domain.dormitory.study.entity.StudyApplicationStatus
 import team.incube.flooding.domain.dormitory.study.adapter.StudyRedisAdapter
+import team.incube.flooding.domain.dormitory.study.repository.StudyBanJpaRepository
 import team.incube.flooding.domain.dormitory.study.service.StudyApplicationService
 import team.incube.flooding.global.security.util.CurrentUserProvider
 import team.themoment.sdk.exception.ExpectedException
+import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Service
 @Transactional
 class StudyApplicationServiceImpl (
     private val studyRedisAdapter: StudyRedisAdapter,
+    private val studyBanJpaRepository: StudyBanJpaRepository,
     private val currentUserProvider: CurrentUserProvider,
     private val studyProperties: StudyProperties
 ) : StudyApplicationService {
 
     override fun execute() {
 
-        var user = currentUserProvider.getCurrentUser()
+        val user = currentUserProvider.getCurrentUser()
 
         val now = LocalTime.now()
 
@@ -29,11 +32,15 @@ class StudyApplicationServiceImpl (
             throw ExpectedException("자습 신청 시간이 아닙니다.", HttpStatus.BAD_REQUEST)
         }
         val status = studyRedisAdapter.getApplicationStatus(user.id)
-        if(status == StudyApplicationStatus.BANNED) {
+        if(status == StudyApplicationStatus.BANNED ||
+            studyBanJpaRepository.existsByUserAndBannedUntilAfter(user, LocalDateTime.now())) {
             throw ExpectedException("자습 금지 상태입니다.", HttpStatus.FORBIDDEN)
         }
-        if(status == StudyApplicationStatus.APPROVED || status == StudyApplicationStatus.CANCELLED) {
+        if(status == StudyApplicationStatus.APPROVED) {
             throw ExpectedException("이미 자습을 신청했습니다.", HttpStatus.CONFLICT)
+        }
+        if(status == StudyApplicationStatus.CANCELLED) {
+            throw ExpectedException("자습을 취소하여 신청하지 못합니다.", HttpStatus.CONFLICT)
         }
 
         val newCount = studyRedisAdapter.incrementCount()
