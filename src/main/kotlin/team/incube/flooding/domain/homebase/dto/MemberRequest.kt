ab9b5e11d52cbabc381c62ddbb41863e1940dd87
user <sooooooobinn@gmail.com>
2026-03-09 02:10:55 +0900

    update: 사용하지 않는 필드 제거

--- src/main/kotlin/team/incube/flooding/domain/homebase/dto/MemberRequest.kt
+++
@@ -1,7 +0,0 @@
-package team.incube.flooding.domain.homebase.dto
-
-class MemberRequest (
-
-    val studentNumber: String,
-    val username: String
-)
---
+++ src/main/kotlin/team/incube/flooding/domain/homebase/dto/request/CreateHomebaseMemberRequest.kt
@@ -0,0 +1,5 @@
+package team.incube.flooding.domain.homebase.dto.request
+
+class CreateHomebaseMemberRequest (
+    val username: String
+)
