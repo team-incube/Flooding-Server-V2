2bf212a188108000b43cf97c46801a510ffa8dbe
user <sooooooobinn@gmail.com>
2026-03-12 20:19:42 +0900

    update: GetHomebaseMemberResponse -> MemberDto 변경

--- src/main/kotlin/team/incube/flooding/domain/homebase/dto/request/CreateHomebaseRequest.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/dto/request/CreateHomebaseRequest.kt
@@ -1,8 +1,10 @@
 package team.incube.flooding.domain.homebase.dto.request
 
+import team.incube.flooding.domain.homebase.dto.MemberDto
+
 data class CreateHomebaseRequest (
     val period: Int,
     val floor: Int,
     val tableNumber: Int,
-    val members: List<CreateHomebaseMemberRequest>
+    val members: List<MemberDto>
 )
--- src/main/kotlin/team/incube/flooding/domain/homebase/dto/response/GetHomebaseResponse.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/dto/response/GetHomebaseResponse.kt
@@ -1,9 +1,11 @@
 package team.incube.flooding.domain.homebase.dto.response
 
+import team.incube.flooding.domain.homebase.dto.MemberDto
+
 data class GetHomebaseResponse (
     val homebaseId: Long,
     val floor: Int,
     val tableNumber: Int,
     val isAttended: Boolean,
-    val members: List<GetHomebaseMemberResponse>
+    val members: List<MemberDto>
 )
