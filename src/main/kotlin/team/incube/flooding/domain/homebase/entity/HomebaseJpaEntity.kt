747bd073fa9955ffd293ed9cb254b99ae2880a62
user <sooooooobinn@gmail.com>
2026-03-16 20:03:26 +0900

    update: period, isAttended 삭제 및 capcity 추가

--- src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseJpaEntity.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseJpaEntity.kt
@@ -1,4 +1,5 @@
 package team.incube.flooding.domain.homebase.entity
+
 import jakarta.persistence.*
 
 @Entity
@@ -9,15 +10,12 @@ class HomebaseJpaEntity(
     @field:GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
-    @field:Column(name = "period", nullable = false)
-    val period: Int,
-
-    @field:Column(name = "floor", nullable = false)
+    @field:Column(nullable = false)
     val floor: Int,
 
     @field:Column(name = "table_number", nullable = false)
     val tableNumber: Int,
 
-    @field:Column(name = "is_attended", nullable = false)
-    val isAttended: Boolean = false
+    @field:Column(nullable = false)
+    val capacity: Int
 )
