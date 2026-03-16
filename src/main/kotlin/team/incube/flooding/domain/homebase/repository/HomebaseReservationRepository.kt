75b7dfa6e58c2459c2a91eb4e143f2034c0fb900
user <sooooooobinn@gmail.com>
2026-03-16 20:05:30 +0900

    add: HomebaseReservationRepository 추가

---
+++ src/main/kotlin/team/incube/flooding/domain/homebase/repository/HomebaseReservationRepository.kt
@@ -0,0 +1,24 @@
+package team.incube.flooding.domain.homebase.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import team.incube.flooding.domain.homebase.entity.HomebaseReservationJpaEntity
+
+interface HomebaseReservationRepository :
+    JpaRepository<HomebaseReservationJpaEntity, Long> {
+
+    @Query(
+        """
+        SELECT r FROM HomebaseReservationJpaEntity r
+        WHERE r.homebase.id = :homebaseId
+        AND r.startPeriod <= :endPeriod
+        AND r.endPeriod >= :startPeriod
+        """
+    )
+    fun findOverlappingReservation(
+        @Param("homebaseId") homebaseId: Long,
+        @Param("startPeriod") startPeriod: Int,
+        @Param("endPeriod") endPeriod: Int
+    ): List<HomebaseReservationJpaEntity>
+}
