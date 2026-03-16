9da45fd571b0511c2039c881e86c414f76e51855
user <sooooooobinn@gmail.com>
2026-03-16 20:04:34 +0900

    update: 쿼리문 추가

--- src/main/kotlin/team/incube/flooding/domain/homebase/repository/HomebaseMemberRepository.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/repository/HomebaseMemberRepository.kt
@@ -1,14 +1,25 @@
 package team.incube.flooding.domain.homebase.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import team.incube.flooding.domain.homebase.entity.HomebaseMemberJpaEntity
 
-interface HomebaseMemberRepository : JpaRepository<HomebaseMemberJpaEntity, Long> {
+interface HomebaseMemberRepository :
+    JpaRepository<HomebaseMemberJpaEntity, Long> {
 
-    fun findByHomebaseId(homebaseId: Long): List<HomebaseMemberJpaEntity>
-
-    fun existsByStudentNumberAndHomebase_Period(
-        studentNumber: String,
-        period: Int
+    @Query(
+        """
+        SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END
+        FROM HomebaseMemberJpaEntity m
+        WHERE m.studentNumber = :studentNumber
+        AND m.reservation.startPeriod <= :endPeriod
+        AND m.reservation.endPeriod >= :startPeriod
+        """
+    )
+    fun existsStudentReservationOverlap(
+        @Param("studentNumber") studentNumber: String,
+        @Param("startPeriod") startPeriod: Int,
+        @Param("endPeriod") endPeriod: Int
     ): Boolean
 }
