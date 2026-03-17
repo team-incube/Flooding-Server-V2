5dae401e0f377f4fdaffc3c8d8d0416192028914
user <sooooooobinn@gmail.com>
2026-03-17 14:12:17 +0900

    delete: GetHomebaseResponse 삭제

--- src/main/kotlin/team/incube/flooding/domain/homebase/dto/response/GetHomebaseResponse.kt
+++
@@ -1,9 +0,0 @@
-package team.incube.flooding.domain.homebase.dto.response
-
-import team.incube.flooding.domain.homebase.dto.MemberDto
-
-data class GetHomebaseResponse (
-    val homebaseId: Long,
-    val period: Int,
-    val members: List<MemberDto>
-)
