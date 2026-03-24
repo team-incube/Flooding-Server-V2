12759b7492b363b95817ff4486b6f3fefb78755f
user <sooooooobinn@gmail.com>
2026-03-24 18:12:41 +0900

    refactor: 하한선 검증 추가 및 DTO 유효성 검사 적용

--- src/main/kotlin/team/incube/flooding/domain/homebase/dto/request/CreateHomebaseRequest.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/dto/request/CreateHomebaseRequest.kt
@@ -1,9 +1,12 @@
 package team.incube.flooding.domain.homebase.dto.request
 
 import team.incube.flooding.domain.homebase.dto.MemberDto
+import jakarta.validation.constraints.NotEmpty
 
 data class CreateHomebaseRequest(
     val startPeriod: Int,
     val endPeriod: Int,
+
+    @field:NotEmpty(message = "예약 인원은 최소 1명 이상이어야 합니다.")
     val members: List<MemberDto>
 )
