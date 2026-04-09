ce59662cce5b099b6a0625f7542fdba8179900f6
한솔 <hsol73076@gmail.com>
2026-04-09 21:09:17 +0900

    update : ClubMemberService 동아리 리더 확인 로직 추가

--- src/main/kotlin/team/incube/flooding/domain/club/service/ClubMemberService.kt
+++ src/main/kotlin/team/incube/flooding/domain/club/service/ClubMemberService.kt
@@ -9,12 +9,14 @@ import team.incube.flooding.domain.club.entity.ClubParticipantJpaEntity
 import team.incube.flooding.domain.club.repository.ClubJpaRepository
 import team.incube.flooding.domain.club.repository.ClubParticipantJpaRepository
 import team.incube.flooding.domain.user.repository.UserRepository
+import team.incube.flooding.global.security.util.CurrentUserProvider
 
 @Service
 class ClubMemberService(
     private val clubRepository: ClubJpaRepository,
     private val userRepository: UserRepository,
     private val clubParticipantJpaRepository: ClubParticipantJpaRepository,
+    private val currentUserProvider: CurrentUserProvider,
 ) {
     @Transactional
     fun inviteMember(
@@ -30,6 +32,11 @@ class ClubMemberService(
             .orElseThrow {
                 ResponseStatusException(HttpStatus.NOT_FOUND, "ID가 ${clubId}인 동아리를 찾을 수 없습니다.")
             }
+
+        val currentUser = currentUserProvider.getCurrentUser()
+        if (club.leader?.id != currentUser.id) {
+            throw ResponseStatusException(HttpStatus.FORBIDDEN, "동아리 리더만 멤버를 초대할 수 있습니다.")
+        }
 
         val user = userRepository.findById(userId)
             .orElseThrow {
