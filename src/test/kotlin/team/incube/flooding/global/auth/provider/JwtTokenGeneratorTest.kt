51efbfb110be7ffadf6811c44c8f7d40add6105d
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-10 10:36:08 +0900

    test : dormitory, auth 테스트 JUnit5로 복원

--- src/test/kotlin/team/incube/flooding/global/auth/provider/JwtTokenGeneratorTest.kt
+++ src/test/kotlin/team/incube/flooding/global/auth/provider/JwtTokenGeneratorTest.kt
@@ -1,21 +1,19 @@
 package team.incube.flooding.global.auth.provider
 
-import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.Test
+import team.incube.flooding.global.auth.provider.JwtProvider
 
-class JwtTokenGeneratorTest : BehaviorSpec({
-    given("유효한 JWT 설정이 있을 때") {
-        val jwtProvider = JwtProvider(
-            secret = "Kc2Vq9mR7Xw1eP8nZ6dY4sT3hJ5uA0bL2gF9rN1mQ7xW8eP3nZ6dY4tS3hJ5uA0b",
-            accessTokenExpiration = 3600000,
-            refreshTokenExpiration = 604800000,
-        )
+class JwtTokenGeneratorTest {
+    @Test
+    fun generateToken() {
+        val jwtProvider =
+            JwtProvider(
+                secret = "Kc2Vq9mR7Xw1eP8nZ6dY4sT3hJ5uA0bL2gF9rN1mQ7xW8eP3nZ6dY4tS3hJ5uA0b",
+                accessTokenExpiration = 3600000,
+                refreshTokenExpiration = 604800000,
+            )
 
-        `when`("액세스 토큰을 생성하면") {
-            then("정상 생성된다") {
-                val token = jwtProvider.generateAccessToken(userId = 2L)
-                token shouldNotBe null
-            }
-        }
+        val token = jwtProvider.generateAccessToken(userId = 2L)
+        println("Bearer $token")
     }
-})
+}
