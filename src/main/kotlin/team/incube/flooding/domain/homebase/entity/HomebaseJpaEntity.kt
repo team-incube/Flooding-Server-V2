cb210476aa1d7c1773fa75ec5ce9988dfc13e3da
한솔 <hsol73076@gmail.com>
2026-04-10 15:33:56 +0900

    fix : ktlint 포멧팅 적용

--- src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseJpaEntity.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseJpaEntity.kt
@@ -5,17 +5,13 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "tb_homebase")
 class HomebaseJpaEntity(
-
     @field:Id
     @field:GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-
     @field:Column(nullable = false)
     val floor: Int,
-
     @field:Column(name = "table_number", nullable = false)
     val tableNumber: Int,
-
     @field:Column(nullable = false)
-    val capacity: Int
+    val capacity: Int,
 )
