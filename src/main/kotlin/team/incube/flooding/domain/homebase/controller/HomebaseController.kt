05a27076af45431d5922d3299b8024657756e14a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-13 08:48:04 +0900

    Revert "style : homebase 와일드카드 임포트 제거"
    This reverts commit 7602a08fb4a2d384cb132f7b690a763f9e817d08.

--- src/main/kotlin/team/incube/flooding/domain/homebase/controller/HomebaseController.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/controller/HomebaseController.kt
@@ -3,21 +3,11 @@ package team.incube.flooding.domain.homebase.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import team.incube.flooding.domain.homebase.dto.request.CreateHomebaseRequest
 import team.incube.flooding.domain.homebase.dto.request.UpdateHomebaseMembersRequest
 import team.incube.flooding.domain.homebase.dto.response.GetHomebaseResponse
-import team.incube.flooding.domain.homebase.service.CreateHomebaseReservationService
-import team.incube.flooding.domain.homebase.service.DeleteHomebaseReservationService
-import team.incube.flooding.domain.homebase.service.GetHomebaseReservationService
-import team.incube.flooding.domain.homebase.service.PatchHomebaseReservationService
+import team.incube.flooding.domain.homebase.service.*
 import team.themoment.sdk.response.CommonApiResponse
 
 @Tag(name = "홈베이스", description = "홈베이스 예약 관련 API")
