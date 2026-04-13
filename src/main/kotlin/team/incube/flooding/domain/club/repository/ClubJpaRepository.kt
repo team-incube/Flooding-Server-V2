2c15ea3fceb60047c53d6a2b20785c628ae449db
한솔 <hsol73076@gmail.com>
2026-04-13 20:46:29 +0900

    add : 동아리 개설 신청 조회 구현

--- src/main/kotlin/team/incube/flooding/domain/club/repository/ClubJpaRepository.kt
+++ src/main/kotlin/team/incube/flooding/domain/club/repository/ClubJpaRepository.kt
@@ -3,6 +3,9 @@ package team.incube.flooding.domain.club.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import team.incube.flooding.domain.club.entity.ClubJpaEntity
+import team.incube.flooding.domain.club.entity.ClubStatus
 
 @Repository
-interface ClubJpaRepository : JpaRepository<ClubJpaEntity, Long>
+interface ClubJpaRepository : JpaRepository<ClubJpaEntity, Long> {
+    fun findAllByStatus(status: ClubStatus): List<ClubJpaEntity>
+}
