935f5280d038c61a4974250cf8f5d55865ac235e
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-13 11:12:44 +0900

    style : homebase ktlint 수정 및 파일명 변경

--- src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseMemberJpaEntity.kt
+++ src/main/kotlin/team/incube/flooding/domain/homebase/entity/HomebaseMemberJpaEntity.kt
@@ -5,19 +5,15 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "tb_homebase_member")
 class HomebaseMemberJpaEntity(
-
     @field:Id
     @field:GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-
     @field:Column(name = "student_number", nullable = false)
     val studentNumber: String,
-
     @field:Column(nullable = false)
     val name: String,
-
     @field:ManyToOne(fetch = FetchType.LAZY)
     @field:JoinColumn(name = "reservation_id", nullable = false)
     @field:com.fasterxml.jackson.annotation.JsonIgnore
-    val reservation: HomebaseReservationJpaEntity
+    val reservation: HomebaseReservationJpaEntity,
 )
