46d0db04681afdb493877eed95d26b99f8a7bece
한솔 <hsol73076@gmail.com>
2026-04-13 10:55:10 +0900

    update : Swagger 문서화 추가 및 API 경로 복수형 변경

--- src/main/kotlin/team/incube/flooding/domain/club/controller/ClubMemberController.kt
+++ src/main/kotlin/team/incube/flooding/domain/club/controller/ClubMemberController.kt
@@ -1,5 +1,7 @@
 package team.incube.flooding.domain.club.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.incube.flooding.domain.club.service.ClubMemberService
 
+@Tag(name = "동아리 멤버", description = "동아리 멤버 초대 및 권한 위임 등 관리 API")
 @RestController
-@RequestMapping("/club")
+@RequestMapping("/clubs")
 class ClubMemberController(
     private val clubMemberService: ClubMemberService,
 ) {
+
+    @Operation(summary = "멤버 초대", description = "동아리에 새로운 사용자를 멤버로 초대합니다")
     @PostMapping("/{clubId}/member/{userId}")
     @ResponseStatus(HttpStatus.CREATED)
     fun inviteMember(
