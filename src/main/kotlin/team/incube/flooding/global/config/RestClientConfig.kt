6174980f334c8f02f293d212ed41bb6cd3ca07bc
user <sooooooobinn@gmail.com>
2026-04-16 02:52:12 +0900

    feat: RestClientConfig 추가

---
+++ src/main/kotlin/team/incube/flooding/global/config/RestClientConfig.kt
@@ -0,0 +1,12 @@
+package team.incube.flooding.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+@Configuration
+class RestClientConfig {
+    @Bean
+    fun restClientBuilder(): RestClient.Builder = RestClient.builder()
+}
+
