e3cd7d2c43b8bad7235095bab105e060f14ca347
한솔 <hsol73076@gmail.com>
2026-04-19 00:20:44 +0900

    update : CommonApiResponse로 변경

--- src/main/kotlin/team/incube/flooding/domain/club/controller/ClubMemberController.kt
+++ src/main/kotlin/team/incube/flooding/domain/club/controller/ClubMemberController.kt
@@ -1,17 +1,12 @@
-package team.incube.flooding.domain.club.controller
+package team.incube.flooding.domain.club.presentation.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import team.incube.flooding.domain.club.service.ExileMemberService
 import team.incube.flooding.domain.club.service.InviteMemberService
 import team.incube.flooding.domain.club.service.TransferOwnershipService
+import team.themoment.sdk.response.CommonApiResponse
 
 @Tag(name = "동아리 멤버", description = "동아리 멤버 관련 API")
 @RestController
@@ -26,9 +21,9 @@ class ClubMemberController(
     fun inviteMember(
         @PathVariable clubId: Long,
         @PathVariable userId: Long,
-    ): ResponseEntity<Unit> {
+    ): CommonApiResponse<Nothing> {
         inviteMemberService.execute(clubId, userId)
-        return ResponseEntity.noContent().build()
+        return CommonApiResponse.success("OK")
     }
 
     @Operation(summary = "소유권 위임", description = "동아리 방장이 다른 멤버에게 방장 권한을 넘기고 본인의 역할을 변경합니다")
@@ -36,18 +31,18 @@ class ClubMemberController(
     fun transferOwnership(
         @PathVariable clubId: Long,
         @PathVariable targetUserId: Long,
-    ): ResponseEntity<Unit> {
+    ): CommonApiResponse<Nothing> {
         transferOwnershipService.execute(clubId, targetUserId)
-        return ResponseEntity.noContent().build()
+        return CommonApiResponse.success("OK")
     }
 
-    @Operation(summary = "구성원 추방하기", description = "관리자 권한으로 특정 멤버를 동아리에서 추방합니다 ")
+    @Operation(summary = "구성원 추방하기", description = "관리자 권한으로 특정 멤버를 동아리에서 추방합니다")
     @DeleteMapping("/{clubId}/member/exile/{userId}")
     fun exileMember(
         @PathVariable clubId: Long,
         @PathVariable userId: Long,
-    ): ResponseEntity<Unit> {
+    ): CommonApiResponse<Nothing> {
         exileMemberService.execute(clubId, userId)
-        return ResponseEntity.noContent().build()
+        return CommonApiResponse.success("OK")
     }
 }
