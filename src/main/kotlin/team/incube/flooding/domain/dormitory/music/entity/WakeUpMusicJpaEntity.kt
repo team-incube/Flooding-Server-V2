dd739bc0d71875855d4d4c29e3a96fc6bd72d816
seungchan <s24041@gsm.hs.kr>
2026-04-23 17:06:57 +0900

    update : WakeUpMusicJpaEntity OneToOne을 ManyToOne으로 변경 및 title, artist 필드 추가

--- src/main/kotlin/team/incube/flooding/domain/dormitory/music/entity/WakeUpMusicJpaEntity.kt
+++ src/main/kotlin/team/incube/flooding/domain/dormitory/music/entity/WakeUpMusicJpaEntity.kt
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import team.incube.flooding.domain.user.entity.UserJpaEntity
 import java.time.LocalDateTime
@@ -18,11 +18,15 @@ class WakeUpMusicJpaEntity(
     @field:Id
     @field:GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    @field:OneToOne(fetch = FetchType.LAZY)
-    @field:JoinColumn(name = "user_id", nullable = false, unique = true)
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    @field:JoinColumn(name = "user_id", nullable = false)
     val user: UserJpaEntity,
     @field:Column(name = "music_url", nullable = false)
     val musicUrl: String,
+    @field:Column(name = "title", nullable = false)
+    val title: String,
+    @field:Column(name = "artist", nullable = false)
+    val artist: String,
     @field:Column(name = "applied_at", nullable = false)
     val appliedAt: LocalDateTime = LocalDateTime.now(),
 )
