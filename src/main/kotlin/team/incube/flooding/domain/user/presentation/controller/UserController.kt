615332dcbe6d02f7b0e171e0d8c87bca98966249
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-04-24 13:04:27 +0900

    update : 리뷰 반영 - @ApiResponses 제거 및 엔드포인트 /users/me로 변경

--- src/main/kotlin/team/incube/flooding/domain/user/presentation/controller/UserController.kt
+++ src/main/kotlin/team/incube/flooding/domain/user/presentation/controller/UserController.kt
@@ -2,7 +2,6 @@ package team.incube.flooding.domain.user.presentation.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,7 +12,7 @@ import team.themoment.sdk.response.CommonApiResponse
 
 @Tag(name = "유저", description = "유저 관련 API")
 @RestController
-@RequestMapping("/me")
+@RequestMapping("/users")
 class UserController(
     private val getMeService: GetMeService,
 ) {
@@ -21,10 +20,8 @@ class UserController(
         summary = "내 정보 조회",
         description = "현재 로그인한 유저의 정보를 반환합니다.",
     )
-    @ApiResponses(
-        ApiResponse(responseCode = "200", description = "조회 성공"),
-        ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
-    )
-    @GetMapping
+    @ApiResponse(responseCode = "200", description = "조회 성공")
+    @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
+    @GetMapping("/me")
     fun getMe(): CommonApiResponse<GetMeResponse> = CommonApiResponse.success("OK", getMeService.execute())
 }
