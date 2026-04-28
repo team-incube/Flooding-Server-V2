09d7f3f3716902b2e58240a981ceeac6a1ff5883
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-29 08:59:30 +0900

    fix : coroutines-reactor 의존성 추가로 suspend 컨트롤러 빈 응답 버그 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
 
     // External SDK
     implementation("com.github.themoment-team:datagsm-oauth-sdk-java:1.4.1")
