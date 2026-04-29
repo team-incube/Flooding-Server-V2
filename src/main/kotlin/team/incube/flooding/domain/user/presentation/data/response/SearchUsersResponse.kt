0f2c8e9f781d1c807827354275e7cd7ba5d80f28
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-04-29 14:01:52 +0900

    feat : 학생 검색 응답에 성별 정보 추가

--- src/main/kotlin/team/incube/flooding/domain/user/presentation/data/response/SearchUsersResponse.kt
+++ src/main/kotlin/team/incube/flooding/domain/user/presentation/data/response/SearchUsersResponse.kt
@@ -1,10 +1,12 @@
 package team.incube.flooding.domain.user.presentation.data.response
 
+import team.incube.flooding.domain.user.entity.Sex
 import team.incube.flooding.domain.user.entity.UserJpaEntity
 
 data class SearchUsersResponse(
     val id: Long,
     val name: String,
+    val sex: Sex,
     val studentNumber: Int,
     val grade: Int,
     val classNumber: Int,
@@ -15,6 +17,7 @@ data class SearchUsersResponse(
             SearchUsersResponse(
                 id = user.id,
                 name = user.name,
+                sex = user.sex,
                 studentNumber = user.studentNumber,
                 grade = user.grade,
                 classNumber = user.classNumber,
