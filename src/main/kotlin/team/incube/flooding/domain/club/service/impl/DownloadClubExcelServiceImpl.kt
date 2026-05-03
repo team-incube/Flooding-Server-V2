208852a71a766f63e81bded403ede673c07b097e
한솔 <hsol73076@gmail.com>
2026-05-03 20:09:47 +0900

    fix : 데이터 부재 시 엑셀 파일이 깨지는 현상 수정

--- src/main/kotlin/team/incube/flooding/domain/club/service/impl/DownloadClubExcelServiceImpl.kt
+++ src/main/kotlin/team/incube/flooding/domain/club/service/impl/DownloadClubExcelServiceImpl.kt
@@ -64,7 +64,11 @@ class DownloadClubExcelServiceImpl(
         val clubMap = participants.groupBy { it.club.name }
         val clubNames = clubMap.keys.toList()
 
-        if (clubNames.isEmpty()) return
+        if (clubNames.isEmpty()) {
+            val row = sheet.createRow(0)
+            row.createCell(0).setCellValue("조회된 참여 데이터가 없습니다.")
+            return
+        }
 
         val headerRow = sheet.createRow(0)
         clubNames.forEachIndexed { i, name -> headerRow.createCell(i).setCellValue(name) }
