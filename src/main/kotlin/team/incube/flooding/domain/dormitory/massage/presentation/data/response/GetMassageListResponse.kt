958e2d223d056d624a0d7d9f68b656d3a810cc62
jyx-07 <s25069@gsm.hs.kr>
2026-05-16 15:32:24 +0900

    feat : 안마의자 조회 응답에 myApplicationStatus 필드 추가

--- src/main/kotlin/team/incube/flooding/domain/dormitory/massage/presentation/data/response/GetMassageListResponse.kt
+++ src/main/kotlin/team/incube/flooding/domain/dormitory/massage/presentation/data/response/GetMassageListResponse.kt
@@ -1,6 +1,9 @@
 package team.incube.flooding.domain.dormitory.massage.presentation.data.response
 
+import team.incube.flooding.domain.dormitory.massage.entity.MassageApplicationStatus
+
 data class GetMassageListResponse(
     val isApplicationOpen: Boolean,
+    val myApplicationStatus: MassageApplicationStatus?,
     val applicants: List<GetMassageResponse>,
 )
