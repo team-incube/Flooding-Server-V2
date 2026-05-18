1e79d8a9f5f35785b1254cc119d7996d8d8c971b
jyx-07 <s25069@gsm.hs.kr>
2026-05-18 16:00:41 +0900

    feat : SSE 클라이언트 연결 해지 감지 하트비트 추가

--- src/main/kotlin/team/incube/flooding/domain/dormitory/study/adapter/StudyAttendanceSseEmitterRegistry.kt
+++ src/main/kotlin/team/incube/flooding/domain/dormitory/study/adapter/StudyAttendanceSseEmitterRegistry.kt
@@ -1,6 +1,7 @@
 package team.incube.flooding.domain.dormitory.study.adapter
 
 import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import team.incube.flooding.domain.dormitory.study.presentation.data.response.StudyAttendanceEventResponse
@@ -16,6 +17,17 @@ class StudyAttendanceSseEmitterRegistry {
         emitter.onTimeout { emitters.remove(emitter) }
         emitter.onError { emitters.remove(emitter) }
         return emitter
+    }
+
+    @Scheduled(fixedDelay = 30_000L)
+    fun sendHeartbeat() {
+        emitters.forEach { emitter ->
+            try {
+                emitter.send(SseEmitter.event().comment("heartbeat"))
+            } catch (e: Exception) {
+                emitter.completeWithError(e)
+            }
+        }
     }
 
     @Async
