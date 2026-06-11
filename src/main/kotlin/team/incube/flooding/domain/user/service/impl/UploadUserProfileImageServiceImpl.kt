1ee03833fe3a255278d12f2cd44857f27c5094f3
ming._.jun <163676562+MingJK@users.noreply.github.com>
2026-06-11 13:58:47 +0900

    update : 프로필 이미지 업로드 시 detached 엔티티 대신 DB에서 직접 조회

--- src/main/kotlin/team/incube/flooding/domain/user/service/impl/UploadUserProfileImageServiceImpl.kt
+++ src/main/kotlin/team/incube/flooding/domain/user/service/impl/UploadUserProfileImageServiceImpl.kt
@@ -1,5 +1,6 @@
 package team.incube.flooding.domain.user.service.impl
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
@@ -9,6 +10,7 @@ import team.incube.flooding.domain.user.service.UploadUserProfileImageService
 import team.incube.flooding.global.config.FileStorageConstants.USER_PROFILE_IMAGE_SUB_DIR
 import team.incube.flooding.global.security.util.CurrentUserProvider
 import team.incube.flooding.global.util.FileStorageService
+import team.themoment.sdk.exception.ExpectedException
 
 @Service
 class UploadUserProfileImageServiceImpl(
@@ -18,7 +20,11 @@ class UploadUserProfileImageServiceImpl(
 ) : UploadUserProfileImageService {
     @Transactional
     override fun execute(image: MultipartFile): UploadUserProfileImageResponse {
-        val user = currentUserProvider.getCurrentUser()
+        val currentUser = currentUserProvider.getCurrentUser()
+        val user =
+            userRepository
+                .findById(currentUser.id)
+                .orElseThrow { ExpectedException("존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND) }
         fileStorageService.delete(user.profileImageUrl)
         val profileImageUrl = fileStorageService.store(image, USER_PROFILE_IMAGE_SUB_DIR)
         user.profileImageUrl = profileImageUrl
